28dd462c3dda5a586e040c9001e214068e6fa650
robybyte <3768068+robybyte@users.noreply.github.com>
2018-03-29 23:30:54 +0200

    Added Stuff!

---
+++ KeyPress/KeyPress/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace KeyPress
+{
+    public partial class Form1 : Form
+    {
+
+        /*Modifiy this!*/
+        private static int Value = 100;
+        /*End of Modifiy area!!*/
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+        private static Boolean visible = false;
+        private static Form1 form;
+        
+        private static int cnt = 0;
+        public Form1()
+        {
+
+            form = this;
+            InitializeComponent();
+            _hookID = SetHook(_proc);
+            Application.Run();
+            
+          //  UnhookWindowsHookEx(_hookID);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private static  IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                Keys pressed = (Keys)vkCode;
+                if (pressed == Keys.F8)
+                {
+                    while (cnt < Value)
+                    {
+                        SendKeys.Send("W");
+                        SendKeys.Send("D");
+                        cnt++;
+                        Thread.Sleep(1000);
+                    }
+                    if (cnt > Value)
+                        cnt = 0;
+
+                    /* if (!visible)
+                         Form1.form.Show();
+                     else
+                         Form1.form.Hide();
+                     visible = !visible;*/
+                    // hier keommen wir an!
+                }
+                else if (pressed == Keys.Return || pressed == Keys.Tab)
+                    Console.Write(Environment.NewLine);
+                else if (pressed >= Keys.A && pressed <= Keys.Z)
+                    Console.Write(pressed);
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Hide();
+            visible = false;
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+       LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
